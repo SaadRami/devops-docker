ddbb688b795d1c2fed9969800621b9118c7248eb
Valérie <valerie.chantraine@gmail.com>
2021-01-27 15:33:47 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from 'cors'
+
+const app = express();
+const port = 8040;
+// ajout de l'en tête "Access Control Allow Origin"
+app.use(cors());
+
+app.get("/", (req, res) => res.json({
+    date: new Date(),
+    number: Math.round(Math.random() * 10)
+}));
+app.listen(port, () => console.info(`app listening on port ${port}`));
